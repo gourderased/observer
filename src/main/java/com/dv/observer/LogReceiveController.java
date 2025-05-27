69d7699d3ae4841dbf7a21efd266f2aeadd9aa63
gourderased <gourderased@gmail.com>
2025-05-27 11:42:22 +0900

    "Restructure log collection flow"
    - Remove gitlab ci pipeline
    - Added entity, dto, controller, service
    - Verified with curl requests; server processes incoming logs as expected

--- src/main/java/com/dv/observer/LogReceiveController.java
+++ src/main/java/com/dv/observer/LogReceiveController.java
@@ -1,6 +1,5 @@
 package com.dv.observer;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,15 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("api/logs")
+@RequestMapping("/logs")
 public class LogReceiveController {
-
-    private final LogService logService;
 
     @PostMapping
     public ResponseEntity<String> receiveLog(@RequestBody LogReceiveRequestDto requestDto) {
-        logService.save(requestDto);
-        return ResponseEntity.ok("Log saved");
+        System.out.println("Received log: " + requestDto);
+        return ResponseEntity.ok("Log received");
     }
 }
